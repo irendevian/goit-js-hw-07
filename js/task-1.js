6b92fe26c72c1014ae1af82623ac657fba1ddb68
Iryna Devianina <devianinairen@gmail.com>
2025-07-08 20:15:44 +0300

    start js task-1

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+const categories = document.querySelectorAll('.item');
+const countCateg = categories.length;
+console.log(`Number of categories: ${countCateg}`);
+
+// const nameCategories = document.querySelector('h2');
+// console.log(`Category: ${nameCategories.textContent}`);
+
+// for (const category of categories) {
+//     console.log(`Category: ${category.textContent}`);
+//     console.log(`Elements: ${category.children.length}`);
+// }
+
