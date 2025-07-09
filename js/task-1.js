c90a1ead3e8d651e3b642ef4f492c6ba13e7e8bb
Iryna Devianina <devianinairen@gmail.com>
2025-07-09 17:54:05 +0300

    finish js task-1

--- js/task-1.js
+++ js/task-1.js
@@ -2,11 +2,20 @@ const categories = document.querySelectorAll('.item');
 const countCateg = categories.length;
 console.log(`Number of categories: ${countCateg}`);
 
-// const nameCategories = document.querySelector('h2');
+
+// -------------------------------------------------------------
+
+// const nameCategories = document.querySelectorAll('h2');
 // console.log(`Category: ${nameCategories.textContent}`);
 
-// for (const category of categories) {
-//     console.log(`Category: ${category.textContent}`);
-//     console.log(`Elements: ${category.children.length}`);
-// }
+// const sublistItem = document.querySelectorAll('ul .list-item');
+// const sublistCount = sublistItem.children;
+// console.log(sublistCount);
+
+// ---------------------------------------------------------
+
+for (const category of categories) {
+    console.log(`Category: ${category.querySelector('h2').textContent}`);
+    console.log(`Elements: ${category.querySelectorAll('ul .list-item').length}`);
+}
 
