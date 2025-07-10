c4ee9402dfdf93dd360b3c77a92e7c2327e1f296
Iryna Devianina <devianinairen@gmail.com>
2025-07-10 23:27:37 +0300

    add js for task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const email = event.target.elements.email.value.trim();
+    const password = event.target.elements.password.value.trim()
+    if (email === "" || password === "") {
+        alert("All form fields must be filled in");
+    } else {
+        const info = {
+            Email: email,
+            Password: password
+        }
+
+        console.log(info);
+        form.reset();
+    }
+
+}
