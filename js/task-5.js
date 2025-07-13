41d0f2aefae226bf10a50d688f6a3e96ce83abfb
Iryna Devianina <devianinairen@gmail.com>
2025-07-13 17:02:39 +0300

    add js for task-5

--- js/task-5.js
+++ js/task-5.js
@@ -4,3 +4,14 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
+const body = document.querySelector('body');
+const button = document.querySelector('button');
+const span = document.querySelector('.color')
+
+button.addEventListener('click', () => {
+
+  body.style.backgroundColor = getRandomHexColor();
+  span.textContent = body.style.backgroundColor;
+});
+
+
